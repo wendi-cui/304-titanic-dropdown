6b76db63fc9124b1e8e1519503bb00911849d92e
wendic <wendi_cui@intuit.com>
2022-03-16 23:27:53 +0000

    make changes

--- app.py
+++ app.py
@@ -49,31 +49,31 @@ app.layout = html.Div([
 @app.callback(Output('display-value', 'figure'),
               [Input('dropdown', 'value')])
 def display_value(continuous_var):
-    grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
+    grouped_mean=df.groupby(['Embarked', 'Sex'])[continuous_var].mean()
     results=pd.DataFrame(grouped_mean)
     # Create a grouped bar chart
     mydata1 = go.Bar(
-        x=results.loc['first'].index,
-        y=results.loc['first'][continuous_var],
-        name='First Class',
+        x=results.loc['Southampton'].index,
+        y=results.loc['Southampton'][continuous_var],
+        name='Southampton',
         marker=dict(color=color1)
     )
     mydata2 = go.Bar(
-        x=results.loc['second'].index,
-        y=results.loc['second'][continuous_var],
-        name='Second Class',
+        x=results.loc['Cherbourg'].index,
+        y=results.loc['Cherbourg'][continuous_var],
+        name='Cherbourg',
         marker=dict(color=color2)
     )
     mydata3 = go.Bar(
-        x=results.loc['third'].index,
-        y=results.loc['third'][continuous_var],
-        name='Third Class',
+        x=results.loc['Queenstown'].index,
+        y=results.loc['Queenstown'][continuous_var],
+        name='Queenstown',
         marker=dict(color=color3)
     )
 
     mylayout = go.Layout(
         title='Grouped bar chart',
-        xaxis = dict(title = 'Port of Embarkation'), # x-axis label
+        xaxis = dict(title = 'Sex'), # x-axis label
         yaxis = dict(title = str(continuous_var)), # y-axis label
 
     )
